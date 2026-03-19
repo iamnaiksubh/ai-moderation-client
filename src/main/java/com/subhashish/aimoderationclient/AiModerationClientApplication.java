58f9496e29826c75889ebd58a34d038ad6e6d768
iamnaiksubh <subhashishnaik1@gmail.com>
2026-03-19 12:48:52 +0530

    initial commit

---
+++ src/main/java/com/subhashish/aimoderationclient/AiModerationClientApplication.java
@@ -0,0 +1,13 @@
+package com.subhashish.aimoderationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiModerationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiModerationClientApplication.class, args);
+	}
+
+}
